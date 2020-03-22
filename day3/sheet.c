ec63b0e5916513c1251e74e81080c480eb2922ad
yamatcha <sojusojuonetmrlarcbz@gmail.com>
2020-03-22 23:40:25 +0900

    finish 11-4

--- day3/sheet.c
+++ day3/sheet.c
@@ -13,9 +13,10 @@ struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize
     ctl->xsize = xsize;
     ctl->ysize = ysize;
     ctl->top = -1;
-    for (i = 0; i > MAX_SHEETS; i++)
+    for (i = 0; i < MAX_SHEETS; i++)
     {
         ctl->sheets0[i].flags = 0;
+        ctl->sheets0[i].ctl = ctl;
     }
 err:
     return ctl;
@@ -47,8 +48,9 @@ void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, i
     return;
 }
 
-void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
+void sheet_updown(struct SHEET *sht, int height)
 {
+    struct SHTCTL *ctl = sht->ctl;
     int h, old = sht->height;
     if (height > ctl->top + 1)
     {
@@ -110,33 +112,33 @@ void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
     return;
 }
 
-void sheet_refresh(struct SHTCTL *ctl, struct SHEET *sht, int bx0, int by0, int bx1, int by1)
+void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1)
 {
     if (sht->height >= 0)
     {
-        sheet_refreshsub(ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1);
+        sheet_refreshsub(sht->ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1);
     }
     return;
 }
 
-void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0)
+void sheet_slide(struct SHEET *sht, int vx0, int vy0)
 {
     int old_vx0 = sht->vx0, old_vy0 = sht->vy0;
     sht->vx0 = vx0;
     sht->vy0 = vy0;
     if (sht->height >= 0)
     {
-        sheet_refreshsub(ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize);
-        sheet_refreshsub(ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize);
+        sheet_refreshsub(sht->ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize);
+        sheet_refreshsub(sht->ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize);
     }
     return;
 }
 
-void sheet_free(struct SHTCTL *ctl, struct SHEET *sht)
+void sheet_free(struct SHEET *sht)
 {
     if (sht->height >= 0)
     {
-        sheet_updown(ctl, sht, -1);
+        sheet_updown(sht, -1);
     }
     sht->flags = 0;
     return;

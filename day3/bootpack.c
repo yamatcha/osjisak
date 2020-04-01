4dad5bd500d6a1b16abb7fce205e9579c86e95ac
yamatcha <sojusojuonetmrlarcbz@gmail.com>
2020-04-01 14:36:31 +0900

    finish 18-2

--- day3/bootpack.c
+++ day3/bootpack.c
@@ -214,12 +214,17 @@ void HariMain(void)
                         key_to = 1;
                         make_wtitle8(buf_win, sht_win->bxsize, "task_a", 0);
                         make_wtitle8(buf_cons, sht_cons->bxsize, "console", 1);
+                        cursor_c = -1;
+                        boxfill8(sht_win->buf, sht_win->bxsize, COL8_FFFFFF, cursor_x, 28, cursor_x + 7, 43);
+                        fifo32_put(&task_cons->fifo, 2);
                     }
                     else
                     {
                         key_to = 0;
                         make_wtitle8(buf_win, sht_win->bxsize, "task_a", 1);
                         make_wtitle8(buf_cons, sht_cons->bxsize, "console", 0);
+                        cursor_c = COL8_000000;
+                        fifo32_put(&task_cons->fifo, 3);
                     }
                     sheet_refresh(sht_win, 0, 0, sht_win->bxsize, 21);
                     sheet_refresh(sht_cons, 0, 0, sht_cons->bxsize, 21);
@@ -267,7 +272,10 @@ void HariMain(void)
                     wait_KBC_sendready();
                     io_out8(PORT_KEYDAT, keycmd_wait);
                 }
-                boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+                if (cursor_c >= 0)
+                {
+                    boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+                }
                 sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
             }
             else if (512 <= i && i <= 767)
@@ -321,16 +329,25 @@ void HariMain(void)
                 if (i != 0)
                 {
                     timer_init(timer, &fifo, 0);
-                    cursor_c = COL8_000000;
+                    if (cursor_c >= 0)
+                    {
+                        cursor_c = COL8_000000;
+                    }
                 }
                 else
                 {
                     timer_init(timer, &fifo, 1);
-                    cursor_c = COL8_FFFFFF;
+                    if (cursor_c >= 0)
+                    {
+                        cursor_c = COL8_FFFFFF;
+                    }
                 }
                 timer_settime(timer, 50);
-                boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
-                sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
+                if (cursor_c >= 0)
+                {
+                    boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+                    sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
+                }
             }
         }
     }
@@ -454,7 +471,7 @@ void console_task(struct SHEET *sheet)
     struct TIMER *timer;
     struct TASK *task = task_now();
 
-    int i, fifobuf[128], cursor_x = 16, cursor_c = COL8_000000;
+    int i, fifobuf[128], cursor_x = 16, cursor_c = -1;
     char s[2];
 
     fifo32_init(&task->fifo, 128, fifobuf, task);
@@ -481,14 +498,29 @@ void console_task(struct SHEET *sheet)
                 if (i != 0)
                 {
                     timer_init(timer, &task->fifo, 0);
-                    cursor_c = COL8_FFFFFF;
+                    if (cursor_c >= 0)
+                    {
+                        cursor_c = COL8_FFFFFF;
+                    }
                 }
                 else
                 {
                     timer_init(timer, &task->fifo, 1);
-                    cursor_c = COL8_000000;
+                    if (cursor_c >= 0)
+                    {
+                        cursor_c = COL8_000000;
+                    }
                 }
                 timer_settime(timer, 50);
+            }
+            if (i == 2)
+            {
+                cursor_c = COL8_FFFFFF;
+            }
+            if (i == 3)
+            {
+                boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cursor_x, 28, cursor_x + 7, 43);
+                cursor_c = -1;
             }
             if (256 <= i && i <= 511)
             {
@@ -511,7 +543,10 @@ void console_task(struct SHEET *sheet)
                     }
                 }
             }
-            boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+            if (cursor_c >= 0)
+            {
+                boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+            }
             sheet_refresh(sheet, cursor_x, 28, cursor_x + 8, 44);
         }
     }

1d97ae9c46d1adc74becc4597ae12312d7f93de6
yamatcha <sojusojuonetmrlarcbz@gmail.com>
2020-04-04 20:49:51 +0900

    finish 22-6

--- day3/console.c
+++ day3/console.c
@@ -318,6 +318,7 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
     char name[13], *p, *q;
     struct TASK *task = task_now();
     int i;
+    int segsiz, datsiz, esp, dathrb;
 
     for (i = 0; i < 8; i++)
     {
@@ -342,48 +343,68 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
     if (finfo != 0)
     {
         p = (char *)memman_alloc_4k(memman, finfo->size);
-        q = (char *)memman_alloc_4k(memman, 64 * 1024);
-        *((int *)0xfe8) = (int)p;
         file_loadfile(finfo->clustno, finfo->size, p, fat, (char *)(ADR_DISKIMG + 0x003e00));
-        set_segmdesc(gdt + 1003, finfo->size - 1, (int)p, AR_CODE32_ER + 0x60);
-        set_segmdesc(gdt + 1004, 64 * 1024 - 1, (int)q, AR_DATA32_RW + 0x60);
-        if (finfo->size >= 8 && strcmp(p + 4, "Hari", 4) == 0)
+        if (finfo->size >= 8 && strcmp(p + 4, "Hari", 4) == 0 && *p == 0x00)
         {
-            start_app(0x1b, 1003 * 8, 64 * 1024, 1004 * 8, &(task->tss.esp0));
+            segsiz = *((int *)(p + 0x0000));
+            esp = *((int *)(p + 0x000c));
+            datsiz = *((int *)(p + 0x0010));
+            dathrb = *((int *)(p + 0x0014));
+            q = (char *)memman_alloc_4k(memman, segsiz);
+            *((int *)0xfe8) = (int)q;
+            set_segmdesc(gdt + 1003, finfo->size - 1, (int)p, AR_CODE32_ER + 0x60);
+            set_segmdesc(gdt + 1004, segsiz - 1, (int)q, AR_DATA32_RW + 0x60);
+            for (i = 0; i < datsiz; i++)
+            {
+                q[esp + i] = p[dathrb + i];
+            }
+            start_app(0x1b, 1003 * 8, esp, 1004 * 8, &(task->tss.esp0));
+            memman_free_4k(memman, (int)q, segsiz);
         }
         else
         {
-            start_app(0, 1003 * 8, 64 * 1024, 1004 * 8, &(task->tss.esp0));
+            cons_putstr0(cons, ".hrb file format error. \n");
         }
-        start_app(0, 1003 * 8, 64 * 1024, 1004 * 8, &(task->tss.esp0));
         memman_free_4k(memman, (int)p, finfo->size);
-        memman_free_4k(memman, (int)q, 64 * 1024);
         cons_newline(cons);
         return 1;
     }
     return 0;
 }
 
-int hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax)
+int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax)
 {
-    int cs_base = *((int *)0xfe8);
+    int ds_base = *((int *)0xfe8);
     struct TASK *task = task_now();
     struct CONSOLE *cons = (struct CONSOLE *)*((int *)0x0fec);
+    struct SHTCTL *shtctl = (struct CONSOLE *)*((int *)0x0fe4);
+    struct SHEET *sht;
+    int *reg = &eax + 1;
+    char s[12];
     if (edx == 1)
     {
         cons_putchar(cons, eax & 0xff, 1);
     }
     else if (edx == 2)
     {
-        cons_putstr0(cons, (char *)ebx + cs_base);
+        cons_putstr0(cons, (char *)ebx + ds_base);
     }
     else if (edx == 3)
     {
-        cons_putstr1(cons, (char *)ebx, ecx + cs_base);
+        cons_putstr1(cons, (char *)ebx, ecx + ds_base);
     }
     else if (edx == 4)
     {
         return &(task->tss.esp0);
+    }
+    else if (edx == 5)
+    {
+        sht = sheet_alloc(shtctl);
+        sheet_setbuf(sht, (char *)ebx + ds_base, esi, edi, eax);
+        make_window8((char *)ebx + ds_base, esi, edi, (char *)ecx + ds_base, 0);
+        sheet_slide(sht, 100, 50);
+        sheet_updown(sht, 3);
+        reg[7] = (int)sht;
     }
     return 0;
 }
